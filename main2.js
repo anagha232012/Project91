290b7741e34c2f6f5ae3dc83e9fef1bed827e56e
anagha232012 <91827774+anagha232012@users.noreply.github.com>
2022-06-06 07:31:23 +0100

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,16 @@
+player_1name = localStorage.getItem("player1_name");
+player_2name = localStorage.getItem("player2_name");
+
+function send() {
+  number1 = document.getElementById("number1").value;
+  number2 = document.getElementById("number2").value;
+  actual_answer = parseInt(number1) * parseInt(number2);
+  question_number = "<h4>" + number1 + "X" + number2 + "</h4>";
+  input_box = "<br> Anwer : <input type = 'text' id = 'input_check_box'>";
+  check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+  row = question_number + input_box + check_button ;
+  document.getElementById("output").innerHTML = row;
+  document.getElementById("number1").value = "";
+  document.getElementById("number2").value = "";
+
+}
